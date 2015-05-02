89e94c4e9b1f1a1ac3e4931cc70653e5def9d711
Javi <javier@sportmaniacs.com>
2015-05-02 11:41:02 -0700

    save to image method

--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -22,6 +22,38 @@
             var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
             return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
         }
+    },
+    saveImageToPhone: function (url, success, error) {
+        var canvas, context, imageDataUrl, imageData;
+        var img = new Image();
+        img.onload = function() {
+            canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+            try {
+                imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
+                imageData = imageDataUrl.replace(/data:image\/jpeg;base64,/, '');
+                cordova.exec(
+                    success,
+                    error,
+                    'Canvas2ImagePlugin',
+                    'saveImageDataToLibrary',
+                    [imageData]
+                );
+            }
+            catch(e) {
+                error(e.message);
+            }
+        };
+        try {
+            img.src = url;
+        }
+        catch(e) {
+            error(e.message);
+        }
     }
+
   };
   
